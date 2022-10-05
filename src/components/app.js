dba0892efd7f1c285243bc7a4d0162fa09ddf625
Renaud Genevois <renaud.genevois@insee.fr>
2022-10-05 17:55:16 +0200

    index

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,6 @@
+import { useCallback, useState } from "react";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
 import CssBaseline from "@mui/material/CssBaseline";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
@@ -9,21 +11,68 @@ import MenuIcon from "@mui/icons-material/Menu";
 import "./application.scss";
 import * as React from "react";
 import Stack from "@mui/material/Stack";
+import TaskIndex from "./task-index";
+
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 
 const theme = createTheme();
 
-function timedTask(task) {
-  if (typeof task === "function") {
-    const start = new Date();
-    const results = task();
-    const end = new Date();
-    const ellapsed = end.getTime() - start.getTime();
-    return { ellapsed, results };
-  }
+async function fetchPcs2020() {
+  const data = await fetch(`${window.location.href}/json/pcs2020.json`).then(
+    (r) => r.json()
+  );
+
+  return data;
 }
 
-function fetchPcs2020() {
-  return fetch("/json/pcs2020.json").then((r) => r.json());
+function fetchCorePcs2020() {
+  return fetch(`${window.location.href}/json/pcs2020-core.json`).then((r) =>
+    r.json()
+  );
+}
+
+const STATUS = {
+  ready: 0,
+  loaded: 1,
+  terminated: 2,
+};
+
+function CreateIndex() {
+  const [data, setData] = useState(undefined);
+  const [storeInfo, setStoreInfo] = useState(undefined);
+  const [status, setStatus] = useState(STATUS.ready);
+
+  const onClick = useCallback(
+    function () {
+      (async function () {
+        if (!storeInfo) {
+          setStoreInfo(await fetchCorePcs2020());
+        }
+        if (!data) {
+          setData(await fetchPcs2020());
+        }
+        setStatus(STATUS.loaded);
+      })();
+    },
+    [data, storeInfo]
+  );
+
+  // useEffect(function () {}, [data, storeInfo]);
+
+  if (status !== 0 && data && storeInfo) {
+    return (
+      <Paper style={{ width: "270px", padding: "5px 5px" }}>
+        <TaskIndex data={data} storeInfo={storeInfo} version="1" />
+      </Paper>
+    );
+  }
+
+  return (
+    <Button variant="text" onClick={onClick}>
+      Index!
+    </Button>
+  );
 }
 
 function App() {
@@ -48,8 +97,17 @@ function App() {
         </Toolbar>
       </AppBar>
       <Stack spacing={2} direction="row">
-        <Button variant="text">Text</Button>
+        <CreateIndex />
       </Stack>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          "& > :not(style)": { m: 1 },
+        }}
+      >
+        <TextField helperText="pcs2020" label="PCS2020" />
+      </Box>
     </ThemeProvider>
   );
 }
---
+++ src/components/task-index.js
@@ -0,0 +1,73 @@
+import { useState, useEffect, useCallback } from "react";
+import LinearProgress from "@mui/material/LinearProgress";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import useStoreIndex from "../js/store-tools/use-store-index";
+import { clearStoreData } from "../js/store-tools";
+import createAppendTask from "../js/suggester-workers/append-to-index/index";
+import updateStoreInfo from "../js/store-tools/update-store-info";
+
+const STATUS = {
+  ready: 0,
+  running: 1,
+  terminated: 2,
+};
+
+function TaskIndex({ storeInfo, data, version }) {
+  const [percent, setPercent] = useState(0);
+  const [status, setStatus] = useState(STATUS.ready);
+  const [startTime, setStartTime] = useState(undefined);
+
+  const db = useStoreIndex(storeInfo, "1");
+
+  const log = useCallback(function (info) {
+    const { percent } = info?.message;
+    if (percent !== undefined) {
+      setPercent(percent);
+
+      if (percent === 100) {
+        setStatus(STATUS.terminated);
+      }
+    }
+  }, []);
+
+  useEffect(
+    function () {
+      let abort_ = () => null;
+      (async function () {
+        if (db && status === STATUS.ready) {
+          setStatus(STATUS.running);
+          await clearStoreData(db);
+          await updateStoreInfo(db, storeInfo);
+          const [index, abort] = createAppendTask(storeInfo, version, log);
+          abort_ = abort;
+
+          setStartTime(new Date().getTime());
+          await index(data);
+        }
+      })();
+      return () => abort_();
+    },
+    [data, db, storeInfo, version, status, log]
+  );
+
+  return (
+    <>
+      <Box sx={{ width: "100%" }}>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <Box sx={{ width: "100%", mr: 1 }}>
+            <LinearProgress variant="determinate" value={percent} />
+          </Box>
+          <Box sx={{ minWidth: 35 }}>
+            <Typography variant="body2" color="text.secondary">{`${Math.round(
+              percent
+            )}%`}</Typography>
+          </Box>
+        </Box>
+      </Box>
+      <div>{`${new Date().getTime() - startTime} ms`}</div>
+    </>
+  );
+}
+
+export default TaskIndex;
